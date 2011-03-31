5d8f3db0233ab6a65792495a8b6df95827d77706
Wes Garland <wes@page.ca>
2011-03-31 15:08:08 -0400

    Bumped JS_GetImplementationVersion date

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -1140,7 +1140,7 @@ JS_ToggleOptions(JSContext *cx, uint32 options)
 JS_PUBLIC_API(const char *)
 JS_GetImplementationVersion(void)
 {
-    return "JavaScript-C 1.8.5 2011-03-24";
+    return "JavaScript-C 1.8.5 2011-03-31";
 }
 
 JS_PUBLIC_API(JSCompartmentCallback)
